519f216167f4613e1755b08433042d5886fd9576
Tranvansu2001 <Tranvansu2001@gmail.com>
2022-06-09 18:01:18 +0700

    built Header #1

--- src/components/Layout/component/Header/index.js
+++ src/components/Layout/component/Header/index.js
@@ -1,5 +1,13 @@
 import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleXmark,
+  faMagnifyingGlass,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
+
 import styles from "./Header.module.scss";
+import images from "~/assets/images";
 
 const cx = classNames.bind(styles);
 
@@ -7,9 +15,20 @@ const Header = () => {
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
-        {/* logo  
-          search
-        */}
+        <div className={cx("logo")}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+        <div className={cx("search")}>
+          <input placeholder="Search account and videos" spellCheck={false} />
+          <button className={cx("clear")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+          <button className={cx("search-btn")}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx("action")}>action</div>
       </div>
     </header>
   );
