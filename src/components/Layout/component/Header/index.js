a0fd7f09150a208140889b4009f25b6cdf6fe8bb
Tranvansu2001 <Tranvansu2001@gmail.com>
2022-06-09 15:54:23 +0700

    defaultLayout

--- src/components/Layout/component/Header/index.js
+++ src/components/Layout/component/Header/index.js
@@ -1,7 +1,18 @@
-import React from "react";
+import classNames from "classnames/bind";
+import styles from "./Header.module.scss";
+
+const cx = classNames.bind(styles);
 
 const Header = () => {
-  return <h2>Header</h2>;
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        {/* logo  
+          search
+        */}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
